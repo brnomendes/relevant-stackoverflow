47b30ab92efb900d314ca62b2587d1e294807779
Bruno Mendes <brunosouza@alunos.utfpr.edu.br>
2020-01-30 18:40:22 -0300

    Add front end libs

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask
 from server.routes import Routes
 from api.sail_client import SAILClient
 
-
 if __name__ == "__main__":
     flask_app = Flask(__name__)
     api_client = SAILClient()
