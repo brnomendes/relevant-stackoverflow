e51e979664c7d7862dd496c7c64f8b8df3773ce3
Bruno Mendes <brunosouza@alunos.utfpr.edu.br>
2020-01-31 03:22:16 -0300

    Add configurations for heroku deploy

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from flask import Flask
 from server.routes import Routes
 from api.sail_client import SAILClient
 
-if __name__ == "__main__":
-    flask_app = Flask(__name__)
-    api_client = SAILClient()
+flask_app = Flask(__name__)
 
-    routes = Routes(flask_app, api_client)
-    routes.configure()
-    routes.run(debug=True)
+api_client = SAILClient()
+routes = Routes(flask_app, api_client)
+routes.configure()
+
+if __name__ == "__main__":
+    routes.run()
